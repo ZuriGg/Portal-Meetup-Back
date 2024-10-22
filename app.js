0a41050de354f58c87ecdfb6b34d794f73096212
Paula Fernández <better_days1996@hotmail.com>
2024-10-22 19:40:14 +0200

    implementando getPool y modificaciones en .env

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+    res.send('¡Hola, mundo!');
 });
 
 // Iniciar el servidor en el puerto 3000
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
---
+++ src/database/getPool.js
@@ -0,0 +1,42 @@
+import mysql from 'mysql2/promise';
+
+// Variables de entorno
+import { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } from '../../env.js';
+
+// Variable para el pool de conexiones.
+let pool;
+
+// Función que retorna un pool de conexiones
+const getPool = async () => {
+    try {
+        // Si la variable "pool" es undefined...
+        if (!pool) {
+            // Creamos una pool temporal.
+            const poolTemp = mysql.createPool({
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASS,
+            });
+
+            // Con el pool temporal creamos la base de datos si no existe.
+            await poolTemp.query(`CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`);
+
+            // Creamos un grupo de conexiones.
+            pool = mysql.createPool({
+                connectionLimit: 10,
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASS,
+                database: MYSQL_DB,
+                // Zona horaria del servidor de MySQL
+                timezone: 'local',
+            });
+        }
+
+        return pool;
+    } catch (err) {
+        throw err;
+    }
+};
+
+export default getPool;

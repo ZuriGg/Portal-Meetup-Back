5fffac63ed925daee38af1f4e80640bb19fd3b62
Paula Fernández <better_days1996@hotmail.com>
2024-10-22 10:28:01 +0200

    carpetas principales y configuración básica del entorno. Seguimos el modelo de Luis/Stefano, llamamos a .env en un env.js y luego tenemos un app.js para inicializar el proyecto, mientras en src sólo habrá carpetas

---
+++ env.js
@@ -0,0 +1,16 @@
+// Importar las variables de entorno del .env
+export const {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+    MYSQL_PORT,
+    PORT,
+    SECRET,
+    UPLOADS_DIR,
+    SMTP_SERVICE,
+    SMTP_PORT,
+    SMTP_USER,
+    SMTP_PASS,
+    SMTP_API_KEY,
+} = process.env;

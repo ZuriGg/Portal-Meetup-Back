fcc77d8ed30d8b26eaa38a3a06f2dbee596990f4
dicalmaestra <dicalmaestra@gmail.com>
2024-10-23 19:47:03 +0200

    Error 404  Not Found

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ app.get('/', (req, res) => {
     res.send('¡Hola, mundo!');
 });
 
+//implementacion middelwares de control de ruta no encontrada y de errores
+
+//middleware de manejo de errores
+app.use((error, req, res, next) => {
+    console.log(error);
+
+    res.status(error.httpStatus || 500).send({
+        status: 'error!!!',
+        message: error.message
+    });
+});
+
+//middelware de ruta no encontrada
+app.use((req,res) => {
+    res.status(404).send({
+        status: 'error',
+        message: 'Not Found'
+    });
+});
+
 // Iniciar el servidor en el puerto 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

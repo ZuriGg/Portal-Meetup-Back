51cd9aba357fdea1ce9e52b4b15a4356e8390a0c
dicalmaestra <dicalmaestra@gmail.com>
2024-10-23 20:14:44 +0200

    error 404

--- app.js
+++ app.js
@@ -11,4 +11,22 @@ app.get('/', (req, res) => {
 app.use((error, req, res, next) => {
     console.log(error);
 
-    res
+    res.status(error.httpStatus || 500).send({
+        status: 'error!!!',
+        message: error.message
+    });
+});
+
+//middelware de ruta no encontrada
+app.use((req,res) => {
+    res.status(404).send({
+        status: 'error',
+        message: 'Not Found'
+    });
+});
+
+// Iniciar el servidor en el puerto 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});

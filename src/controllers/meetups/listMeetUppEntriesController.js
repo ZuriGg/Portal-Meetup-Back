777901b8ccde97a5c62cdb793de1004aeaf99fe6
Jonathan <jona_pana13@hotmail.com>
2024-10-26 18:03:55 +0100

    cambio de nombre de archivo del servico de meetup entradas y controlador que manejar el servicio de todas las entradas

--- src/controllers/meetups/listMeetUppEntriesController.js
+++ src/controllers/meetups/listMeetUppEntriesController.js
@@ -1,2 +1,15 @@
+import selectAllEntriesMeetUpService from '../../services/meetups/selectAllEntriesMeetUpService.js';
 
+const listMeetUppEntriesController = async (req, res, next) => {
+    try {
+        const meetUpEntries = selectAllEntriesMeetUpService();
+        res.send({
+            status: 'ok',
+            data: meetUpEntries,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
 
+export default listMeetUppEntriesController;

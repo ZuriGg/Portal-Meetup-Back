eef4d96fb451b994aa82b2bc45753e5f11d43ea1
Paula Fernández <better_days1996@hotmail.com>
2024-10-28 13:46:01 +0100

    implementando newUserSchema.js

---
+++ src/schemas/users/newUserSchema.js
@@ -0,0 +1,19 @@
+import joi from 'joi';
+
+// Mensajes de error personalizados.
+import joiErrorMessages from '../joiErrorMessages.js';
+
+// Esquema donde comprobamos todas las propiedades necesarias.
+const newUserSchema = joi.object({
+    username: joi.string().required().messages(joiErrorMessages),
+    password: joi
+        .string()
+        .pattern(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[¡!$%^&*()_+|~=`{}:";'<>¿?,.])[a-zA-Z0-9¡!$%^&*()_+|~=`{}:";'<>¿?,.]{8,}$/
+        )
+        .required()
+        .messages(joiErrorMessages),
+    email: joi.string().email().required().messages(joiErrorMessages),
+});
+
+export default newUserSchema;

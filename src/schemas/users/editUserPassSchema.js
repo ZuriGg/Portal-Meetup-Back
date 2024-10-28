815aef5314e547720d8524509443f0bfc30b20ad
Paula Fernández <better_days1996@hotmail.com>
2024-10-28 12:24:30 +0100

    implementando editUserPassSchema.js

---
+++ src/schemas/users/editUserPassSchema.js
@@ -0,0 +1,19 @@
+import joi from 'joi';
+
+// Mensajes de error personalizados.
+import joiErrorMessages from '../joiErrorMessages.js';
+
+// Esquema donde comprobamos todas las propiedades necesarias.
+const editUserPassSchema = joi.object({
+    email: joi.string().email().required().messages(joiErrorMessages),
+    recoverPassCode: joi.string().required().messages(joiErrorMessages),
+    newPass: joi
+        .string()
+        .pattern(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[¡!$%^&*()_+|~=`{}:";'<>¿?,.])[a-zA-Z0-9¡!$%^&*()_+|~=`{}:";'<>¿?,.]{8,}$/
+        )
+        .required()
+        .messages(joiErrorMessages),
+});
+
+export default editUserPassSchema;

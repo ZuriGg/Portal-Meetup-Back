db65db4a4be60f954d47a37779c9d999306576ad
Jonathan Méndez Romanelli <82724800+JmendezRoma@users.noreply.github.com>
2024-10-30 14:48:59 +0000

    Delete src/routes/meetupRoutes.js

--- src/routes/meetupRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import meetUpCategoryController from '../controllers/meetups/meetUpCategoryController.js';
-
-const meetUpRouter = express.Router();
-
-meetUpRouter.get('/categories', meetUpCategoryController);
-
-export default meetUpRouter;

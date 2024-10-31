3e4ee9bf09a571b8fa5dd8e863910c79de6b83fd
zuri <zurimanes@gmail.com>
2024-10-31 03:03:56 +0100

    actualizo selectMeetupByIdModel

--- src/models/meetups/selectMeetupByIdModel.js
+++ src/models/meetups/selectMeetupByIdModel.js
@@ -6,32 +6,9 @@ const selectMeetupByIdModel = async (meetupId, userId = '') => {
     const pool = await getPool();
 
     // Obtenemos la información del meetup.
-    const [meetups] = await pool.query(
-        `
-                SELECT 
-                    M.id,
-                    M.title,
-                    M.description,
-                    M.startDate,
-                    M.oneSession,
-                    M.hourMeetup,
-                    M.dayOfTheWeek,
-                    M.aforoMax,
-                    M.userId,
-                    U.username,
-                    BIT_OR(V.userId = ?) AS votedByMe, 
-                    M.userId = ? AS owner,
-                    AVG(IFNULL(V.value, 0)) AS votes,
-                    M.createdAt
-                FROM meetups M
-                LEFT JOIN meetupVotes V ON V.meetupId = M.id
-                INNER JOIN users U ON U.id = M.userId
-                WHERE M.id = ?
-                GROUP BY M.id
-                ORDER BY M.createdAt DESC
-            `,
-        [userId, userId, meetupId]
-    );
+    const [meetups] = await pool.query('SELECT * FROM meetups WHERE id = ?', [
+        meetupId,
+    ]);
 
     // Verifica que el meetup exista
     if (meetups.length === 0) {
@@ -48,11 +25,11 @@ const selectMeetupByIdModel = async (meetupId, userId = '') => {
     meetups[0].photos = photos;
 
     // Establecemos como valores booleanos "votedByMe" y "owner"
-    meetups[0].votedByMe = Boolean(meetups[0].votedByMe);
-    meetups[0].owner = Boolean(meetups[0].owner);
+    /*     meetups[0].votedByMe = Boolean(meetups[0].votedByMe);
+    meetups[0].owner = Boolean(meetups[0].owner); */
 
     // La media de votos es un valor de tipo String. La convertimos a Number.
-    meetups[0].votes = Number(meetups[0].votes) || 0;
+    /*     meetups[0].votes = Number(meetups[0].votes) || 0; */
 
     return {
         ...meetups[0],

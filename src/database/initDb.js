7c872a799c359cd17fac608f74023f9d08b4699c
zuri <zurimanes@gmail.com>
2024-11-02 19:35:39 +0100

    Actualizo las primary key de la base de datos a auto incremento

--- src/database/initDb.js
+++ src/database/initDb.js
@@ -21,7 +21,7 @@ const main = async () => {
         console.log('Borrando tablas...');
 
         await pool.query(
-            'DROP TABLE IF EXISTS users, meetups, meetupPhotos, meetupVotes, location'
+            'DROP TABLE IF EXISTS users, meetups, meetupPhotos, meetupVotes, location, category, attendance, outOfService'
         );
 
         console.log('Creando tablas...');
@@ -29,31 +29,31 @@ const main = async () => {
         // Tabla de usuarios --> información de los usuarios registrados en el sistema
         await pool.query(`
             CREATE TABLE IF NOT EXISTS users (
-                id CHAR(36) PRIMARY KEY NOT NULL,
+                id INT AUTO_INCREMENT PRIMARY KEY,
                 email VARCHAR(100) UNIQUE NOT NULL,
                 username VARCHAR(30) UNIQUE NOT NULL,
                 firstName VARCHAR(30) UNIQUE NOT NULL,
                 lastname VARCHAR(30) UNIQUE NOT NULL,
                 password VARCHAR(100) NOT NULL,
-                meetupOwner VARCHAR(100), /* si es organizador de algún meetup */
-                avatar VARCHAR(100), /* Ruta o URL del avatar del usuario */
-                active BOOLEAN DEFAULT false, /* Estado del usuario, 'false' por defecto. Los admin activan a los organizadores para subir eventos */
-                role ENUM('organizador', 'normal', 'admin') DEFAULT 'normal', /* crear evento desde normal u organizador, pero los admin confirman que esos usuarios son válidos  */
-                registrationCode CHAR(30), /* Código de registro para activación */
-                recoverPassCode CHAR(10), /* Código para recuperar contraseña */
+                meetupOwner VARCHAR(100),
+                avatar VARCHAR(100),
+                active BOOLEAN DEFAULT false,
+                role ENUM('organizador', 'normal', 'admin') DEFAULT 'normal',
+                registrationCode CHAR(30),
+                recoverPassCode CHAR(10),
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
                 modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
-            )	
+            )
         `);
 
-        // Creamos la tabla de location --> guarda la información de ubicaciones para los eventos
+        // Tabla de location --> guarda la información de ubicaciones para los eventos
         await pool.query(`
             CREATE TABLE IF NOT EXISTS location (
-                id CHAR(36) PRIMARY KEY NOT NULL,
+                id INT AUTO_INCREMENT PRIMARY KEY,
                 city VARCHAR(50) NOT NULL,
                 address VARCHAR(100),
-                notes VARCHAR(100) NOT NULL, /* lugar en concreto, ej: sala de usos múltiples */
-                zip CHAR(5) NOT NULL, /* CHAR ocupa menos espacio */
+                notes VARCHAR(100) NOT NULL,
+                zip CHAR(5) NOT NULL,
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP 
             )
         `);
@@ -61,7 +61,7 @@ const main = async () => {
         // Tabla de category --> almacena las diferentes categorías de los eventos
         await pool.query(`
             CREATE TABLE IF NOT EXISTS category (
-                id CHAR(36) PRIMARY KEY NOT NULL,
+                id INT AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(50) NOT NULL
             )
         `);
@@ -69,18 +69,18 @@ const main = async () => {
         // Tabla de meetups --> registra los eventos creados por los usuarios
         await pool.query(`
             CREATE TABLE IF NOT EXISTS meetups (
-                id CHAR(36) PRIMARY KEY NOT NULL,
+                id INT AUTO_INCREMENT PRIMARY KEY,
                 title VARCHAR(50) NOT NULL,
                 description TEXT NOT NULL, 
-                startDate DATE NOT NULL, /* fecha de inicio */
-                oneSession BOOLEAN DEFAULT FALSE, /* ej. determinar si es una única sesión, como el martes 29*/
+                startDate DATE NOT NULL,
+                oneSession BOOLEAN DEFAULT FALSE,
                 hourMeetup TIME,
-                dayOfTheWeek ENUM ('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'), /* NO PONER NOT NULL por si no es un día en concreto*/
+                dayOfTheWeek ENUM ('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'),
                 aforoMax TINYINT UNSIGNED,
-                userId CHAR(36) NOT NULL, /* usuario relacionado (puede ser asistente o co-organizador) */
-                owner CHAR(36) NOT NULL,
-                locationId CHAR(36) NOT NULL,
-                categoryId CHAR(36) NOT NULL,
+                userId INT NOT NULL, /* Ahora es INT para referenciar a users.id */
+                owner INT NOT NULL,
+                locationId INT NOT NULL,
+                categoryId INT NOT NULL,
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
                 modifiedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 FOREIGN KEY (userId) REFERENCES users(id),
@@ -89,12 +89,12 @@ const main = async () => {
             )
         `);
 
-        // Tabla de fotos --> almacena las fotos asociadas a los eventos
+        // Tabla de meetupPhotos --> almacena las fotos asociadas a los eventos
         await pool.query(`
             CREATE TABLE IF NOT EXISTS meetupPhotos (
-                id CHAR(36) PRIMARY KEY NOT NULL,
+                id INT AUTO_INCREMENT PRIMARY KEY,
                 url VARCHAR(100) NOT NULL,
-                meetupId CHAR(36) NOT NULL,
+                meetupId INT NOT NULL,
                 FOREIGN KEY (meetupId) REFERENCES meetups(id),
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
             )
@@ -103,10 +103,10 @@ const main = async () => {
         // Tabla de attendance --> registra la asistencia de los usuarios a los eventos
         await pool.query(`
             CREATE TABLE IF NOT EXISTS attendance (
-                id CHAR(36) PRIMARY KEY NOT NULL,
+                id INT AUTO_INCREMENT PRIMARY KEY,
                 date DATETIME NOT NULL,
-                userId CHAR(36) NOT NULL,
-                meetupId CHAR(36) NOT NULL,
+                userId INT NOT NULL,
+                meetupId INT NOT NULL,
                 FOREIGN KEY (userId) REFERENCES users(id),
                 FOREIGN KEY (meetupId) REFERENCES meetups(id)
             )
@@ -115,22 +115,22 @@ const main = async () => {
         // Tabla de outOfService --> registra la cancelación o inhabilitación de eventos
         await pool.query(`
             CREATE TABLE IF NOT EXISTS outOfService (
-                id CHAR(36) PRIMARY KEY NOT NULL,
-                notes VARCHAR(100) NOT NULL, /* motivo por el que se cancela: aforoMax, huelga de basura */
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                notes VARCHAR(100) NOT NULL,
                 date DATETIME NOT NULL,
-                meetupId CHAR(36) NOT NULL,
+                meetupId INT NOT NULL,
                 FOREIGN KEY (meetupId) REFERENCES meetups(id)
             )
         `);
 
-        // Tabla de votos --> permite que los usuarios califiquen los eventos según asistencia (sesión)
+        // Tabla de meetupVotes --> permite que los usuarios califiquen los eventos según asistencia (sesión)
         await pool.query(`
             CREATE TABLE IF NOT EXISTS meetupVotes (
-                id CHAR(36) PRIMARY KEY NOT NULL,
+                id INT AUTO_INCREMENT PRIMARY KEY,
                 value TINYINT UNSIGNED NOT NULL CHECK (value BETWEEN 1 AND 5), 
                 coment TEXT NOT NULL,
-                userId CHAR(36) NOT NULL,
-                attendanceId CHAR(36) NOT NULL,
+                userId INT NOT NULL,
+                attendanceId INT NOT NULL,
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (userId) REFERENCES users(id),
                 FOREIGN KEY (attendanceId) REFERENCES attendance(id)

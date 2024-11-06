1aefefa0111d20d906ca1feabe148dbb88233578
Paula Fernández <better_days1996@hotmail.com>
2024-11-06 12:33:37 +0100

    implementando getUserProfileController para ver el "perfil público de un usuario"

---
+++ src/controllers/users/getUserProfileController.js
@@ -0,0 +1,37 @@
+import selectUserByIdModel from '../../models/users/selectUserByIdModel.js';
+import { notFoundError } from '../../services/errorService.js';
+
+const getUserProfileController = async (req, res, next) => {
+    try {
+        // id del usuario extraido de path params.
+        const { userId } = req.params;
+
+        // datos del usuario.
+        const user = await selectUserByIdModel(userId);
+
+        if (!user) {
+            // Si no se encuentra el usuario, respondemos con un error 404.
+            throw notFoundError();
+        }
+
+
+        // Eliminamos los datos privados del usuario.
+        delete user.password;  // No enviar la contraseña.
+        delete user.email;     // No enviar el correo electrónico.
+        delete user.registrationCode; // No enviar el código de registro.
+        delete user.recoverPassCode;  // No enviar el código de recuperación de contraseña.
+
+        res.send({
+            status: 'ok',
+            data: {
+                user,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export default getUserProfileController;
+
+// El propósito de esta función es manejar una solicitud para obtener el perfil de un usuario por su userId, asegurándose de eliminar información privada (como el correo electrónico) antes de enviar la respuesta.

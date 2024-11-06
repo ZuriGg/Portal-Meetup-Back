c25dd53f07c114103e9e4ed8a74e9808fab83954
Paula Fernández <better_days1996@hotmail.com>
2024-11-06 21:35:04 +0100

    aplicando pequeña modularización de rutas en app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import fileUpload from 'express-fileupload'; /* librería para subir imágenes */
 import cors from 'cors';
 import morgan from 'morgan';
-import userRouter from './src/routes/userRoutes.js';
-import meetUpRouter from './src/routes/meetupRoutes.js';
 import path from 'path';
+
+// Importamos las rutas.
+import routes from './src/routes/index.js';
+
 
 import {
     errorController,
@@ -29,9 +31,8 @@ app.use(fileUpload());
 const staticDir = path.join(process.cwd(), './src/uploads');
 app.use('/uploads', express.static(staticDir)); //uploads es un directorio estático/endpoint para pedir una imagen
 
-//middlewares de rutas.
-app.use(userRouter);
-app.use(meetUpRouter);
+// Middleware que indica a express dónde están las rutas.
+app.use(routes);
 
 //middleware de manejo de errores
 app.use(errorController);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-// Importamos las rutas de los usuarios y de las meetup.
+// Importamos las rutas de los usuarios y de los meetup.
 import userRoutes from './userRoutes.js';
 import meetupRoutes from './meetupRoutes.js';
 
-// Creamos un router.
+// Creamos un router principal.
 const router = express.Router();
 
 // Indicamos a express dónde están las rutas de los usuarios y los meetup.

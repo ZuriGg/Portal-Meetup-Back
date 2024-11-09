667a0001e70b22075bbcd1ac30ae99ac2ef648e4
Paula Fernández <better_days1996@hotmail.com>
2024-11-09 21:11:19 +0100

    implementando esquema de joi para el endpoint de invalidacion de algun dia

--- src/controllers/meetups/invalidDayMeetupController.js
+++ src/controllers/meetups/invalidDayMeetupController.js
@@ -1,10 +1,19 @@
 import invalidDayMeetupModel from '../../models/meetups/invalidDayMeetupModel.js';
 
+//para validar el body con el esquema proporcionado
+import validateSchemaUtil from '../../utils/validateSchemaUtil.js';
+
+//importamos el esquema concreto
+import invalidDayMeetupSchema from '../../schemas/meetups/invalidDayMeetupSchema.js';
+
 const invalidDayMeetupController = async (req, res, next) => {
     try {
         const { meetupId } = req.params;
         const { date } = req.body;
         const { notes } = req.body;
+
+        // aplicamos la validacion con joi antes de seguir con el controlador
+        await validateSchemaUtil(invalidDayMeetupSchema, req.body);
 
         if (!date) {
             throw new Error(

e1aaed617ad9115b1579c423fbfec5f703fb4e84
Paula Fernández <better_days1996@hotmail.com>
2024-11-09 20:23:43 +0100

    implementamos nuevo controlador addMeetupPhotoController para añadir fotos a los meetups, con su respectivo joi

--- src/schemas/meetups/addMeetupPhotoSchema.js
+++ src/schemas/meetups/addMeetupPhotoSchema.js
@@ -5,7 +5,7 @@ import imgSchema from '../imgSchema.js';
 
 // Esquema donde comprobamos todas las propiedades necesarias.
 const addMeetupPhotoSchema = joi.object({
-    photo: imgSchema.required(),
+    photo: imgSchema.optional(),
 });
 
 export default addMeetupPhotoSchema;

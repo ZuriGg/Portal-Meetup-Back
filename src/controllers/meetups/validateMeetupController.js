73d57a616809317f5235cc6773b264abbf9aa66a
Paula Fernández <better_days1996@hotmail.com>
2024-11-09 20:40:26 +0100

    implementando el esquema de validación joi para el endpoint validateMeetup

--- src/controllers/meetups/validateMeetupController.js
+++ src/controllers/meetups/validateMeetupController.js
@@ -2,12 +2,21 @@ import validateMeetupModel from '../../models/meetups/validateMeetupModel.js';
 
 import { notFoundError } from '../../services/errorService.js';
 
+//para validar el body con el esquema proporcionado
+import validateSchemaUtil from '../../utils/validateSchemaUtil.js';
+
+//importamos el esquema concreto
+import validateMeetupSchema from '../../schemas/meetups/validateMeetupSchema.js';
+
 // Controlador para validar (Hacer público) un meetup
 const validateMeetupController = async (req, res, next) => {
     try {
         const { meetupId } = req.params;
         const { validated } = req.body;
         let mensajeValidacion = '';
+
+        // aplicamos la validacion con joi antes de seguir con el controlador
+        await validateSchemaUtil(validateMeetupSchema, req.body);
 
         const isValidated =
             validated === 'true' ? true : validated === 'false' ? false : null;

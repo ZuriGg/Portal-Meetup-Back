c3ca902599ad924be2cb0eb618186aac01da6bde
Jonathan Méndez Romanelli <jona_pana13@hotmail.com>
2024-11-14 03:47:44 +0000

    cambios version uuid que no hice, solo el console log

--- src/controllers/meetups/listMeetUppController.js
+++ src/controllers/meetups/listMeetUppController.js
@@ -2,11 +2,12 @@ import selectAllMeetupsModel from '../../models/meetups/selectAllMeetupsModel.js
 
 const listMeetUpController = async (req, res, next) => {
     try {
-
         const keyword = req.query;
 
         const meetUpEntries = await selectAllMeetupsModel(keyword);
-        
+
+        console.log(meetUpEntries);
+
         res.send({
             status: 'ok',
             data: meetUpEntries,

0d8bd57fc00fc4cea0a3fdaf47d6bbce5bc271be
zuri <zurimanes@gmail.com>
2024-11-16 23:58:04 +0100

    Actualizo joi de edit meetup

--- src/schemas/meetups/editMeetupSchema.js
+++ src/schemas/meetups/editMeetupSchema.js
@@ -1,43 +1,47 @@
-import joi from 'joi'; // Solo importa Joi como un objeto
+import joi from 'joi';
+
+// Mensajes de error personalizados.
 import joiErrorMessages from '../joiErrorMessages.js';
 
-// Esquema donde comprobamos todas las propiedades necesarias para un nuevo Meetup.
+// Creamos el esquema para comprobar todas las propiedades.
 const newMeetupSchema = joi.object({
     title: joi.string().min(5).max(50).required().messages(joiErrorMessages),
     description: joi.string().min(10).required().messages(joiErrorMessages),
     startDate: joi.date().min('now').required().messages(joiErrorMessages),
-    oneSession: joi
-        .boolean()
-        .default(false)
-        .optional()
-        .messages(joiErrorMessages),
+    oneSession: joi.boolean().default(false).messages(joiErrorMessages),
     categoryId: joi.number().integer().required().messages(joiErrorMessages),
-    hourMeetUp: joi
+    city: joi.string().max(50).required().messages(joiErrorMessages),
+    address: joi.string().max(100).messages(joiErrorMessages),
+    zip: joi
+        .number()
+        .integer()
+        .min(1001) //mínimo para un codigo postal en España
+        .max(52006) //máximo para un código postal en España
+        .required()
+        .messages(joiErrorMessages),
+    hourMeetup: joi
         .string()
         .pattern(/^([01]?[0-9]|2[0-3]):([0-5]?[0-9])$/) // HH:MM
-        .optional()
         .messages(joiErrorMessages),
     dayOfTheWeek: joi
         .string()
         .valid(
             'lunes',
             'martes',
-            'miércoles',
+            'miercoles',
             'jueves',
             'viernes',
-            'sábado',
+            'sabado',
             'domingo'
         )
-        .optional()
         .messages(joiErrorMessages),
     aforoMax: joi
         .number()
-        .integer()
-        .min(0) // Valor mínimo para unsigned
-        .max(255) // Máximo de 255
-        .optional()
+        .integer() //número entero
+        .min(0)
+        .max(80000) // grandes eventos como conciertos, festivales etc
         .messages(joiErrorMessages),
-    userId: joi.number().integer().optional().messages(joiErrorMessages),
+    locationId: joi.number().integer(), //número entero
 });
 
 export default newMeetupSchema;

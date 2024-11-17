812f3f7d0aa471f0b686630e0bcb706817b3e1c3
Paula Fernández <better_days1996@hotmail.com>
2024-11-17 17:10:12 +0100

    convirtiendo en opcionales los campos de modificacion

--- src/schemas/users/editUserSchema.js
+++ src/schemas/users/editUserSchema.js
@@ -3,11 +3,10 @@ import joiErrorMessages from '../joiErrorMessages.js';
 
 // Esquema donde comprobamos todas las propiedades necesarias.
 const editUserSchema = joi.object({
-    firstName: joi.string().required().messages(joiErrorMessages),
-    lastname: joi.string().required().messages(joiErrorMessages),
-    email: joi.string().email().required().messages(joiErrorMessages),
-    username: joi.string().required().messages(joiErrorMessages),
+    firstName: joi.string().optional().messages(joiErrorMessages),
+    lastname: joi.string().optional().messages(joiErrorMessages),
+    email: joi.string().email().optional().messages(joiErrorMessages),
+    username: joi.string().optional().messages(joiErrorMessages),
 });
 
 export default editUserSchema;
-//eliminamos todos los .required() porque la idea es que sea un edit opcional

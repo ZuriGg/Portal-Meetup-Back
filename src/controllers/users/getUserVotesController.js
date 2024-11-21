5400a2cc9e3cbcdd19be9571ea318f03438180dd
Paula Fernández <better_days1996@hotmail.com>
2024-11-21 10:24:49 +0100

    añadimos el controlador de usuario para conseguir todos sus votos

---
+++ src/controllers/users/getUserVotesController.js
@@ -0,0 +1,19 @@
+import selectVotesByUserIdModel from '../../models/meetups/selectVotesByUserIdModel.js';
+
+const getUserVotesController = async (req, res, next) => {
+    try {
+        const userId = req.user.id; // Obtenemos el ID del usuario autenticado
+
+        // Obtenemos las valoraciones hechas por el usuario
+        const votes = await selectVotesByUserIdModel(userId);
+
+        res.send({
+            status: 'ok',
+            votes,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export default getUserVotesController;
